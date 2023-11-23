0d2f7d68a4d374a0428177178f480ef0a17f9168
ChamodiWickramasinghe96 <150574967+ChamodiWickramasinghe96@users.noreply.github.com>
2023-11-23 11:40:40 +0530

    Dash Board form design completed & CSS file added.

--- src/controller/DashBoardFormController.java
+++ src/controller/DashBoardFormController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashBoardFormController {
+    public AnchorPane dashBordFormContext;
+
+    public void setUI(String viewName) throws IOException {
+        Stage stage= (Stage) dashBordFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+viewName+".fxml"))));
+    }
+
+    public void customerBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void itemBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void logOutBtnOnAction(ActionEvent actionEvent) {
+
+    }
 }
